ad59940159615e0aecea8b7b5bc9bdeb01590d14
Edelsheim <skyline7651@gmail.com>
2023-01-11 22:50:08 +0900

    upload project init

---
+++ src/main/java/com/example/msghub/MsghubApplication.java
@@ -0,0 +1,11 @@
+package com.example.msghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsghubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsghubApplication.class, args);
+	}
+}
